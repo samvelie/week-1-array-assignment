909e0d318225b7451eb3051cb06f8b121f9ba65f
samvelie <sam.velie@gmail.com>
2017-02-07 17:47:03 -0600

    2nd commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is to demonstrate a single function in javascript that concatenates all of the items from an array starting at a given index point. The index.html file initiates the project for a web browser, linking it to my JavaScript source file. The client.js file is the JavaScript file. Most of the action can be seen in the console on the web browser for this project.
+
+I hope this is the kind of thing one wants to see in a README! Thank you for reading.
--- client.js
+++ client.js
@@ -1,16 +1,20 @@
 //Creating the test arrays here
 var arrayTestOne = ['ASDF', 'BLAH', 'Oh! ', 'Hi ', 'There!'];
 var arrayTestTwo = ['Word', 'Time', 'Now', 'Baby'];
+var arrayTestThree = ['Super ', 'Bad ', 'the ', 'movie ', 'is ', 'pretty ', 'funny ']
 
 //Running the console log tests here, inputting my created arrays and arbitrary index numbers
 console.log(concatenatorFromPoint(arrayTestOne, 3));
 console.log(concatenatorFromPoint(arrayTestOne, 2));
 console.log(concatenatorFromPoint(arrayTestOne, 0));
 console.log(concatenatorFromPoint(arrayTestTwo, 2));
+console.log(concatenatorFromPoint(arrayTestThree, 0));
 
+/* The concatenating function itself. This loops over the array starting at the
+input number a's index, and outputs a string composed of the array's remaining elements */
 function concatenatorFromPoint(someArray, a) {
-  var arrayLength = someArray.length;
-  var newString = ''
+  var arrayLength = someArray.length; //sets this variable for readability
+  var newString = '' //creating an empty string variable that will be filled with the array's elements
   for(var i= a; i<arrayLength; i++){
     newString = newString + someArray[i];
   }
